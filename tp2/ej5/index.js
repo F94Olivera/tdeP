5762992c306c1a6407704be9bec5d01fe1cff9e6
Facu Olivera <f94olivera@gmail.com>
2025-09-07 17:33:18 -0300

    tp2 ej5

---
+++ tp2/ej5/index.js
@@ -0,0 +1,16 @@
+// 5). realizar un algoritmo que muestre por pantalla todos los números que hay entre 1 y 50. Luego muestre por pantalla todos
+// los números entre 150 y 105 en orden decreciente.
+
+console.log("ej5");
+
+console.log("En orden creciente de 1 a 50")
+for (let i = 1; i <= 50; i++){
+    console.log("i: ", i);
+}
+
+console.log("En orden decreciente de 150 a 105")
+let i = 150;
+while (i >= 105){
+    console.log("i: ", i);
+    i--;
+}
