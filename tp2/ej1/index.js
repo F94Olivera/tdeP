7c35467bd18d3dce9fe2a48ed4a324fcbcc52365
Facu Olivera <f94olivera@gmail.com>
2025-09-07 00:06:55 -0300

    tp2 ej1

--- tp2/ej1/index.js
+++ tp2/ej1/index.js
@@ -1,1 +1,13 @@
-// TODO
+// 1). Realizar un programa que muestre en pantalla los números del 1 al 10 y luego del 10 al 1.
+
+console.log("ej1");
+
+for (let i = 1; i < 11; i++){
+    console.log("i: ", i)
+}
+console.log("*************************************")
+let i = 10
+while( i > 0 ){
+    console.log("i: ", i)
+    i--
+}
