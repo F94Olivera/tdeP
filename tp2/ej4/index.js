8bc1458fdc096af198f59fa1bd3923e0c1ae410c
Facu Olivera <f94olivera@gmail.com>
2025-09-07 05:14:45 -0300

    tp2 ej4

---
+++ tp2/ej4/index.js
@@ -0,0 +1,15 @@
+// 4). Dados dos números enteros ingresados por el usuario realizar un algoritmo que calcule el cociente y el resto de su división entera.
+
+console.log("ej4");
+
+const prompt = require("prompt-sync")();
+
+const dividendo = parseInt(prompt("Ingrese dividendo: "))
+const divisor = parseInt(prompt("Ingrese divisor: "))
+
+const result = Math.floor(dividendo/divisor)
+
+const remainder = dividendo % divisor
+
+console.log("result ", result)
+console.log("remainder ", remainder)
